a3359151680a1816191448a27f0796bae8e79fe1
manohar-bharatnxt <manohar.acharya@bharatnxt.in>
2025-08-20 14:58:38 +0530

    6. Replacing String with User Enitity & UserModel

--- lib/features/auth/domain/usecases/user_sign_up.dart
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -1,14 +1,15 @@
 import 'package:blog_app_bloc/core/error/failure.dart';
 import 'package:blog_app_bloc/core/usecase/usecase.dart';
+import 'package:blog_app_bloc/features/auth/domain/entities/user.dart';
 import 'package:blog_app_bloc/features/auth/domain/repository/auth_repository.dart';
 import 'package:fpdart/fpdart.dart';
 
-class UserSignUp implements UseCase<String, UserSignUpParams> {
+class UserSignUp implements UseCase<User, UserSignUpParams> {
   final AuthRepository authRepository;
   const UserSignUp(this.authRepository);
 
   @override
-  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+  Future<Either<Failure, User>> call(UserSignUpParams params) async {
     return await authRepository.signUpWithEmailPassword(
       name: params.name,
       email: params.email,
--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:blog_app_bloc/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:blog_app_bloc/features/auth/domain/entities/user.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
@@ -22,7 +23,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       );
       res.fold(
         (failure) => emit(AuthFailure(failure.message)),
-        (uid) => emit(AuthSucess(uid)),
+        (user) => emit(AuthSucess(user)),
       );
     });
   }
